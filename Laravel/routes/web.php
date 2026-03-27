5c4674f29351dfbe2098e0bdafe30e7aa0b649d3
IanCaleb <ivusaw1227@gmail.com>
2026-03-27 00:22:04 -0300

    logica de rotas para papeis diferentes na aplicação

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+//rotas provisórias sem middleware
+
+Route::get('/manager/dashboard', function () {
+    return view('manager.dashboard');
+});
+
+Route::get('/user/dashboard', function () {
+    return view('user.dashboard');
+});
+
+
+require __DIR__.'/auth.php';
