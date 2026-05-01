2745d323a44cacd0baad4e355690588379a30014
Giselle <gih99freirea123@gmail.com>
2026-05-01 18:41:41 -0300

    refactor: ajusta model e validações de lotes

--- Laravel/app/Models/Batch.php
+++ Laravel/app/Models/Batch.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Batch extends Model
 {
-    //
+    // Permite que essas colunas sejam salvas em massa
+    protected $fillable = [
+        'product_id', 
+        'batch_number', 
+        'quantity', 
+        'entry_date', 
+        'expiration_date'
+    ];
+
+    // Cria o relacionamento com o Produto
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
