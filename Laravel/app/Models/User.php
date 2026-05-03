d0ae8217f8e2a196f05c703cd8d92fd4cb4eec11
Clarasobr <clarasobreira@aluno.unifapce.edu.br>
2026-05-03 14:08:34 -0300

    fix: remove Sanctum trait do Model User para corrigir erro 500 no registro

--- Laravel/app/Models/User.php
+++ Laravel/app/Models/User.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use  HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
