f1f5f2f78293fe906e85d30cc654ca8fed8da52a
IanCaleb <ivusaw1227@gmail.com>
2026-05-04 19:41:55 -0300

    dados mokados de movimentação

--- Laravel/database/Lista-Produtos/movimentacoes.php
+++ Laravel/database/Lista-Produtos/movimentacoes.php
@@ -1,1 +1,524 @@
-{ "id": 1, "name": "Arroz Integral 1kg", "category": "Mercearia", "lote": "ABC-101", "data_validade": "2027-01-15", "data_movimentacao": "2026-03-20", "tipo_movimentacao": "Entrada", "preco_venda": 7.50, "preco_compra": 4.80 },
+<?php
+return [
+    [
+        "id" => 1,
+        "Produto" => [
+            "id" => 1,
+            "name" => "Arroz Integral 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 1,
+                "name" => "L-001",
+                "data_validade" => "2027-01-15",
+            ],
+        ],
+        "data_movimentacao" => "2026-01-05",
+        "tipo_movimentacao" => "compra",
+        "preco_unitário" => 7.50,
+        "quantidade" => 100,
+    ],
+
+    [
+        "id" => 2,
+        "Produto" => [
+            "id" => 2,
+            "name" => "Feijão Carioca 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 2,
+                "name" => "L-002",
+                "data_validade" => "2027-02-10",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-06",
+        "tipo_movimentacao" => "vencimento",
+        "preco_unitário" => 8.20,
+        "quantidade" => 40,
+    ],
+    [
+        "id" => 3,
+        "Produto" => [
+            "id" => 3,
+            "name" => "Macarrão Espaguete 500g",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 3,
+                "name" => "L-003",
+                "data_validade" => "2026-12-01",
+            ],
+        ],
+        "data_movimentacao" => "2026-01-08",
+        "tipo_movimentacao" => "doacao",
+        "preco_unitário" => 4.30,
+        "quantidade" => 120,
+    ],
+    [
+        "id" => 4,
+        "Produto" => [
+            "id" => 4,
+            "name" => "Açúcar Refinado 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 4,
+                "name" => "L-004",
+                "data_validade" => "2027-03-20",
+            ],
+        ],
+        "data_movimentacao" => "2026-03-10",
+        "tipo_movimentacao" => "vencimento",
+        "preco_unitário" => 5.10,
+        "quantidade" => 30,
+    ],
+    [
+        "id" => 5,
+        "Produto" => [
+            "id" => 5,
+            "name" => "Café Torrado 250g",
+            "category" => "Bebidas",
+            "lote" => [
+                "id" => 5,
+                "name" => "L-005",
+                "data_validade" => "2026-11-15",
+            ],
+        ],
+        "data_movimentacao" => "2026-03-12",
+        "tipo_movimentacao" => "vencimento",
+        "preco_unitário" => 6.80,
+        "quantidade" => 60,
+    ],
+    [
+        "id" => 6,
+        "Produto" => [
+            "id" => 6,
+            "name" => "Leite Integral 1L",
+            "category" => "Laticínios",
+            "lote" => [
+                "id" => 6,
+                "name" => "L-006",
+                "data_validade" => "2026-03-15",
+            ],
+        ],
+        "data_movimentacao" => "2026-03-15",
+        "tipo_movimentacao" => "vencimento",
+        "preco_unitário" => 4.90,
+        "quantidade" => 200,
+    ],
+    [
+        "id" => 7,
+        "Produto" => [
+            "id" => 7,
+            "name" => "Óleo de Soja 900ml",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 7,
+                "name" => "L-007",
+                "data_validade" => "2027-04-10",
+            ],
+        ],
+        "data_movimentacao" => "2026-04-18",
+        "tipo_movimentacao" => "vencimento",
+        "preco_unitário" => 7.90,
+        "quantidade" => 50,
+    ],
+    [
+        "id" => 8,
+        "Produto" => [
+            "id" => 8,
+            "name" => "Farinha de Trigo 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 8,
+                "name" => "L-008",
+                "data_validade" => "2027-01-01",
+            ],
+        ],
+        "data_movimentacao" => "2026-01-20",
+        "tipo_movimentacao" => "compra",
+        "preco_unitário" => 5.70,
+        "quantidade" => 90,
+    ],
+    [
+        "id" => 9,
+        "Produto" => [
+            "id" => 9,
+            "name" => "Manteiga 200g",
+            "category" => "Laticínios",
+            "lote" => [
+                "id" => 9,
+                "name" => "L-009",
+                "data_validade" => "2026-03-01",
+            ],
+        ],
+        "data_movimentacao" => "2026-05-22",
+        "tipo_movimentacao" => "vencimento",
+        "preco_unitário" => 9.50,
+        "quantidade" => 20,
+    ],
+    [
+        "id" => 10,
+        "Produto" => [
+            "id" => 10,
+            "name" => "Queijo Mussarela 500g",
+            "category" => "Frios",
+            "lote" => [
+                "id" => 10,
+                "name" => "L-010",
+                "data_validade" => "2026-02-28",
+            ],
+        ],
+        "data_movimentacao" => "2026-01-25",
+        "tipo_movimentacao" => "venda",
+        "preco_unitário" => 18.90,
+        "quantidade" => 35,
+    ],
+
+    // continuei variando padrão ↓
+
+    [
+        "id" => 11,
+        "Produto" => [
+            "id" => 1,
+            "name" => "Arroz Integral 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 11,
+                "name" => "L-011",
+                "data_validade" => "2027-05-10",
+            ],
+        ],
+        "data_movimentacao" => "2026-05-01",
+        "tipo_movimentacao" => "vencimento",
+        "preco_unitário" => 7.80,
+        "quantidade" => 150,
+    ],
+    [
+        "id" => 12,
+        "Produto" => [
+            "id" => 2,
+            "name" => "Feijão Carioca 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 12,
+                "name" => "L-012",
+                "data_validade" => "2027-06-01",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-03",
+        "tipo_movimentacao" => "venda",
+        "preco_unitário" => 8.50,
+        "quantidade" => 70,
+    ],
+    [
+        "id" => 13,
+        "Produto" => [
+            "id" => 3,
+            "name" => "Macarrão Espaguete 500g",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 13,
+                "name" => "L-013",
+                "data_validade" => "2026-10-10",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-05",
+        "tipo_movimentacao" => "compra",
+        "preco_unitário" => 4.60,
+        "quantidade" => 80,
+    ],
+    [
+        "id" => 14,
+        "Produto" => [
+            "id" => 4,
+            "name" => "Açúcar Refinado 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 14,
+                "name" => "L-014",
+                "data_validade" => "2027-07-20",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-07",
+        "tipo_movimentacao" => "ajuste",
+        "preco_unitário" => 5.30,
+        "quantidade" => 25,
+    ],
+    [
+        "id" => 15,
+        "Produto" => [
+            "id" => 5,
+            "name" => "Café Torrado 250g",
+            "category" => "Bebidas",
+            "lote" => [
+                "id" => 15,
+                "name" => "L-015",
+                "data_validade" => "2026-09-01",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-10",
+        "tipo_movimentacao" => "venda",
+        "preco_unitário" => 7.10,
+        "quantidade" => 55,
+    ],
+    [
+        "id" => 16,
+        "Produto" => [
+            "id" => 6,
+            "name" => "Leite Integral 1L",
+            "category" => "Laticínios",
+            "lote" => [
+                "id" => 16,
+                "name" => "L-016",
+                "data_validade" => "2026-03-01",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-12",
+        "tipo_movimentacao" => "compra",
+        "preco_unitário" => 5.00,
+        "quantidade" => 180,
+    ],
+    [
+        "id" => 17,
+        "Produto" => [
+            "id" => 7,
+            "name" => "Óleo de Soja 900ml",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 17,
+                "name" => "L-017",
+                "data_validade" => "2027-08-15",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-15",
+        "tipo_movimentacao" => "compra",
+        "preco_unitário" => 8.10,
+        "quantidade" => 60,
+    ],
+    [
+        "id" => 18,
+        "Produto" => [
+            "id" => 8,
+            "name" => "Farinha de Trigo 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 18,
+                "name" => "L-018",
+                "data_validade" => "2027-02-01",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-18",
+        "tipo_movimentacao" => "compra",
+        "preco_unitário" => 5.90,
+        "quantidade" => 110,
+    ],
+    [
+        "id" => 19,
+        "Produto" => [
+            "id" => 9,
+            "name" => "Manteiga 200g",
+            "category" => "Laticínios",
+            "lote" => [
+                "id" => 19,
+                "name" => "L-019",
+                "data_validade" => "2026-04-10",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-20",
+        "tipo_movimentacao" => "ajuste",
+        "preco_unitário" => 9.80,
+        "quantidade" => 15,
+    ],
+    [
+        "id" => 20,
+        "Produto" => [
+            "id" => 10,
+            "name" => "Queijo Mussarela 500g",
+            "category" => "Frios",
+            "lote" => [
+                "id" => 20,
+                "name" => "L-020",
+                "data_validade" => "2026-03-15",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-22",
+        "tipo_movimentacao" => "venda",
+        "preco_unitário" => 19.20,
+        "quantidade" => 45,
+    ],
+    [
+        "id" => 21,
+        "Produto" => [
+            "id" => 1,
+            "name" => "Arroz Integral 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 21,
+                "name" => "L-021",
+                "data_validade" => "2027-09-01",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-25",
+        "tipo_movimentacao" => "compra",
+        "preco_unitário" => 8.00,
+        "quantidade" => 130,
+    ],
+    [
+        "id" => 22,
+        "Produto" => [
+            "id" => 2,
+            "name" => "Feijão Carioca 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 22,
+                "name" => "L-022",
+                "data_validade" => "2027-10-10",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-27",
+        "tipo_movimentacao" => "vencimento",
+        "preco_unitário" => 8.70,
+        "quantidade" => 65,
+    ],
+    [
+        "id" => 23,
+        "Produto" => [
+            "id" => 3,
+            "name" => "Macarrão Espaguete 500g",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 23,
+                "name" => "L-023",
+                "data_validade" => "2026-11-20",
+            ],
+        ],
+        "data_movimentacao" => "2026-03-01",
+        "tipo_movimentacao" => "compra",
+        "preco_unitário" => 4.80,
+        "quantidade" => 95,
+    ],
+    [
+        "id" => 24,
+        "Produto" => [
+            "id" => 4,
+            "name" => "Açúcar Refinado 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 24,
+                "name" => "L-024",
+                "data_validade" => "2027-11-30",
+            ],
+        ],
+        "data_movimentacao" => "2026-03-03",
+        "tipo_movimentacao" => "ajuste",
+        "preco_unitário" => 5.40,
+        "quantidade" => 35,
+    ],
+    [
+        "id" => 25,
+        "Produto" => [
+            "id" => 5,
+            "name" => "Café Torrado 250g",
+            "category" => "Bebidas",
+            "lote" => [
+                "id" => 25,
+                "name" => "L-025",
+                "data_validade" => "2026-08-15",
+            ],
+        ],
+        "data_movimentacao" => "2026-03-05",
+        "tipo_movimentacao" => "venda",
+        "preco_unitário" => 7.30,
+        "quantidade" => 75,
+    ],
+
+    [
+        "id" => 26,
+        "Produto" => [
+            "id" => 1,
+            "name" => "Arroz Integral 1kg",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 1,
+                "name" => "L-001",
+                "data_validade" => "2027-01-15",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-05",
+        "tipo_movimentacao" => "venda",
+        "preco_unitário" => 7.50,
+        "quantidade" => 100,
+    ],
+
+    [
+        "id" => 27,
+        "Produto" => [
+            "id" => 5,
+            "name" => "Café Torrado 250g",
+            "category" => "Bebidas",
+            "lote" => [
+                "id" => 27,
+                "name" => "L-027",
+                "data_validade" => "2026-08-15",
+            ],
+        ],
+        "data_movimentacao" => "2026-03-05",
+        "tipo_movimentacao" => "compra",
+        "preco_unitário" => 7.30,
+        "quantidade" => 75,
+    ],
+
+    [
+        "id" => 28,
+        "Produto" => [
+            "id" => 5,
+            "name" => "Café Torrado 250g",
+            "category" => "Bebidas",
+            "lote" => [
+                "id" => 27,
+                "name" => "L-027",
+                "data_validade" => "2026-08-15",
+            ],
+        ],
+        "data_movimentacao" => "2026-04-05",
+        "tipo_movimentacao" => "venda",
+        "preco_unitário" => 7.30,
+        "quantidade" => 75,
+    ],
+
+    [
+        "id" => 29,
+        "Produto" => [
+            "id" => 3,
+            "name" => "Macarrão Espaguete 500g",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 23,
+                "name" => "L-023",
+                "data_validade" => "2026-11-20",
+            ],
+        ],
+        "data_movimentacao" => "2026-04-01",
+        "tipo_movimentacao" => "venda",
+        "preco_unitário" => 4.80,
+        "quantidade" => 95,
+    ],
+
+    [
+        "id" => 30,
+        "Produto" => [
+            "id" => 7,
+            "name" => "Óleo de Soja 900ml",
+            "category" => "Mercearia",
+            "lote" => [
+                "id" => 17,
+                "name" => "L-017",
+                "data_validade" => "2027-08-15",
+            ],
+        ],
+        "data_movimentacao" => "2026-02-15",
+        "tipo_movimentacao" => "venda",
+        "preco_unitário" => 8.10,
+        "quantidade" => 60,
+    ],
+];
+
+?>
