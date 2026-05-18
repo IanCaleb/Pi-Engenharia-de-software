8164848d6ea73e27c96b06476ff9647360b70b06
IanCaleb <ivusaw1227@gmail.com>
2026-05-18 19:16:33 -0300

    altração das relações entre as classes produtos e movimentações

--- Laravel/app/Models/Product.php
+++ Laravel/app/Models/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Movement;
 use Carbon\Carbon;
 
 class Product extends Model
@@ -22,5 +23,10 @@ class Product extends Model
     public function batches(): HasMany
     {
         return $this->hasMany(Batch::class);
+    }
+
+    public function movements()
+    {
+        return $this->hasMany(Movement::class);
     }
 }
